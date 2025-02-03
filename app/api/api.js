03e22faaffdea05cf8b6f6c345d5736e67647390
Zakariya06 <zakariyakhanamar00@gmail.com>
2025-02-03 20:14:53 +0500

    latest code for netlify

--- app/api/api.js
+++ app/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/proxy", // Use Next.js API proxy instead of the original API
+  baseURL: "http://api.expo.win/api",
   headers: {
     "Content-Type": "application/json",
   },
