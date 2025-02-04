dfbde1befe319611739bbe62c9a3170260bab5f6
Zakariya06 <zakariyakhanamar00@gmail.com>
2025-02-04 18:27:09 +0500

    change the url

--- app/api/api.js
+++ app/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "/api/proxy/api",
+  baseURL: "https://api.expo.win/api",
   headers: {
     "Content-Type": "application/json",
   },
